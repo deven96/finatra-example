3fd0e792276ba5ebdefd9e4b7ad44584a5c3b5d9
Diretnan Domnan <diretnandomnan@gmail.com>
2020-03-30 15:03:03 +0100

    Initial contribution

---
+++ src/main/scala/com/example/hello/HelloWorldController.scala
@@ -0,0 +1,15 @@
+package com.example.hello
+
+import com.twitter.finagle.http.Request
+import com.twitter.finatra.http.Controller
+
+class HelloWorldController extends Controller {
+
+  get("/hi") { request: Request =>
+    "Hello " + request.params.getOrElse("name", "unnamed")
+  }
+
+  post("/hi") { hiRequest: HiRequest =>
+    "Hello " + hiRequest.name + " with id " + hiRequest.id
+  }
+}
---
+++ src/main/scala/com/example/hello/HiRequest.scala
@@ -0,0 +1,5 @@
+package com.example.hello
+
+case class HiRequest(
+  id: Long,
+  name: String)
